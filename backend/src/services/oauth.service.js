fbff25e18fe79dd4d1054ab757a33445f55e527c
ANHBHN2 <anhbhn2@fpt.com>
2025-09-17 23:15:05 +0700

    lastest

--- backend/src/services/oauth.service.js
+++ backend/src/services/oauth.service.js
@@ -11,7 +11,7 @@ const handleGoogleCallback = (req, res, next) => {
     if (err || !user) {
         const errorMessage = info?.message || "Đăng nhập OAuth thất bại";
         return res.redirect(
-            `${process.env.PUBLIC_SERVER_ENDPOINT}/login?error=${encodeURIComponent(errorMessage)}`
+            `${process.env.FRONTEND_URL}/login?error=${encodeURIComponent(errorMessage)}`
         );
     }
     // this part set 2 token like usual
@@ -42,7 +42,7 @@ const handleGoogleCallback = (req, res, next) => {
         sameSite: 'none',
         maxAge:ms(process.env.REFRESH_TOKEN_LIFE)}) ;// convert to ms
        console.log(`Callback URL with access token: ${process.env.FRONTEND_URL}/oauth-callback?access_token=${accessToken}`)
-    res.redirect(`${process.env.PUBLIC_SERVER_ENDPOINT}/oauth-callback?access_token=${accessToken}`);
+    res.redirect(`${process.env.FRONTEND_URL}/oauth-callback?access_token=${accessToken}`);
 
 })(req, res, next)
 }
